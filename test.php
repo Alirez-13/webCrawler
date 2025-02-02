ba87869df0b8239fc7b67167186ab422163809b3
mehrzad sajjadi <canadavancouver2003@gmail.com>
2025-02-02 16:08:04 +0330

    test.php complete

--- test.php
+++ test.php
@@ -3,7 +3,6 @@ include_once("WebScraper.php");
 $url = "https://www.entekhab.ir";
 $word = "مجلس";
 
-
 $html = file_get_contents($url);
 
 if (str_contains($html, $word)) {
@@ -29,8 +28,28 @@ if (str_contains($html, $word)) {
     $subDomains = $htmlDOM->find("a");
     foreach ($subDomains as $link) {
         // Find all links and add main base domain to fist of subDomain
-        $targetLinks[] = "https://www.entekhab.ir" . $link->href;
+        if (str_contains($link->href, "/fa/news")) {
+            // Append base domain to sub
+            $targetLinks[] = "https://www.entekhab.ir" . $link->href;
+        }
     }
+
+    echo count($targetLinks) . "<br>";
+    $finalLinks = array();
+    foreach ($targetLinks as $key => $link) {
+        // Extract HTML
+        $content = $webScraper->scrape($link);
+        $removedTag = strip_tags($content);
+
+        if (stripos($removedTag, $word)) {
+            echo str_word_count($content);
+            echo "<br>";
+            echo count($content->find("img"));
+            $finalLinks[] = $link;
+
+        }
+    }
+//    print_r($finalLinks);
 
 }
 
